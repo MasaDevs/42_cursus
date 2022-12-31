8e55887238f2a10310991d8d7bf205d06013c0cb
Masahito Arai <masahitoarai@pop-os.localdomain>
2022-12-31 13:08:54 +0900

    is it ok?

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: marai <marai@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 14:43:17 by marai             #+#    #+#             */
-/*   Updated: 2022/12/31 12:37:29 by marai            ###   ########.fr       */
+/*   Updated: 2022/12/31 13:07:59 by marai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,65 @@
 #include <signal.h>
 #include <sys/types.h>
 
+pid_t	check_error(int argc, char *argv[]);
+int		send_char_bit(pid_t pid, char *argv[], int index);
+
 int	main(int argc, char *argv[])
 {
 	pid_t	pid;
-	int		chr;
 	ssize_t	i;
-	ssize_t	j;
 
-	if (argc != 3)
-		return (-1);
-	pid = ft_atoi(argv[1]);
+	pid = check_error(argc, argv);
 	if (pid < 0)
 		return (-1);
 	i = 0;
 	while (argv[2][i] != '\0')
 	{
-		j = 7;
-		chr = argv[2][i];
-		while (0 <= j)
-		{
-			if ((chr >> j) & 1)
-				kill(pid, 17);
-			else
-				kill(pid, 16);
-			usleep(100);
-			j--;
-		}
+		if (send_char_bit(pid, argv, i) == -1)
+			return (-1);
 		i++;
 	}
+}
+
+pid_t	check_error(int argc, char *argv[])
+{
+	pid_t	pid;
+
+	if (argc != 3)
+	{
+		ft_printf("Incorrect the number of arguments\n");
+		return (-1);
+	}
+	pid = ft_atoi(argv[1]);
+	if (pid < 0)
+	{
+		ft_printf("Invalid process id\n");
+		return (-1);
+	}
+	return (pid);
+}
+
+int	send_char_bit(pid_t pid, char *argv[], int index)
+{
+	int	j;
+	int	chr;
+	int	result;
+
+	j = 7;
+	chr = argv[2][index];
+	while (0 <= j)
+	{
+		if ((chr >> j) & 1)
+			result = kill(pid, 17);
+		else
+			result = kill(pid, 16);
+		if (result == -1)
+		{
+			ft_printf("Can't send the messege\nPlease check the pid.\n");
+			return (-1);
+		}
+		usleep(100);
+		j--;
+	}
+	return (1);
 }
